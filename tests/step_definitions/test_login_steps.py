e7fdf261efca56249f0204468495c754d46e7853
jagadish <jagadishm875@gmail.com>
2025-11-02 12:19:12 +0530

    Latest

--- tests/step_definitions/test_login_steps.py
+++ tests/step_definitions/test_login_steps.py
@@ -19,7 +19,7 @@ def open_login_page():
 @when("I enter valid credentials")
 def enter_credentials():
     page.fill("#email", "test@example.com")     # use your test email
-    page.fill("#pass", "Password123")           # use your test passwosrd
+    page.fill("#pass", "Password123hhhhh")           # use your test passwosrdssss
     page.click("button.login")
 
 @then("I should see the dashboard")
